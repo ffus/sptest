6c1e5830d8195f228101d54b6119a017b7677c55
ffus <130901988+ffus@users.noreply.github.com>
2024-03-23 17:19:48 +0800

    Update sptest.sh

--- sptest.sh
+++ sptest.sh
@@ -1,47 +1,45 @@
 #!/bin/bash
 
-# Check if speedtest is installed
-if command -v speedtest &> /dev/null
-then
-    echo "Speedtest is already installed. Running speedtest..."
-    speedtest
-    exit 0
-fi
-
 # Function to install Speedtest CLI on Debian/Ubuntu
-function install_speedtest_debian {
-    # If migrating from prior bintray install instructions please first...
-    sudo rm /etc/apt/sources.list.d/speedtest.list
-    sudo apt-get update
-    sudo apt-get remove -y speedtest
-    # Other non-official binaries will conflict with Speedtest CLI
-    # Example how to remove using apt-get
-    sudo apt-get remove -y speedtest-cli
-    sudo apt-get install -y curl
+install_speedtest_debian() {
+    apt install -y curl
     curl -s https://packagecloud.io/install/repositories/ookla/speedtest-cli/script.deb.sh | sudo bash
-    sudo apt-get install -y speedtest
+    apt install -y speedtest
 }
 
 # Function to install Speedtest CLI on CentOS/RHEL
-function install_speedtest_centos {
-    # If migrating from prior bintray install instructions please first...
-    sudo rm /etc/yum.repos.d/bintray-ookla-rhel.repo
-    sudo yum remove -y speedtest
-    # Other non-official binaries will conflict with Speedtest CLI
-    # Example how to remove using yum
-    rpm -qa | grep speedtest | xargs -I {} sudo yum -y remove {}
+install_speedtest_centos () {
     curl -s https://packagecloud.io/install/repositories/ookla/speedtest-cli/script.rpm.sh | sudo bash
-    sudo yum install -y speedtest
+    yum install -y speedtest
 }
 
-if [ -f "/etc/debian_version" ]; then
-    install_speedtest_debian
-elif [ -f "/etc/redhat-release" ]; then
-    install_speedtest_centos
-else
-    echo "This script only supports Debian/Ubuntu and CentOS/RHEL."
-    exit 1
-fi
+# function for uninstalling speedtest
+uninstall_speedtest() {
+    if [ -f "/etc/debian_version" ]; then
+        rm /etc/apt/sources.list.d/ookla_speedtest-cli.list
+        apt remove -y speedtest
+    elif [ -f "/etc/redhat-release" ]; then
+        rm /etc/yum.repos.d/ookla_speedtest-cli.repo
+        yum remove -y speedtest
+    else
+        echo "Unsupported OS."
+        exit 1
+    fi
+}
 
-# Run speedtest
-speedtest
+# check the argument passed to the script
+if [ $1 == "-u" ]; then
+    uninstall_speedtest
+    echo -e "Uninstall done. \033[0;32m[OK]\033[0m"
+else
+    if [ -f "/etc/debian_version" ]; then
+        install_speedtest_debian
+        yes | speedtest
+    elif [ -f "/etc/redhat-release" ]; then
+        install_speedtest_centos
+        yes | speedtest
+    else
+        echo "This script only supports Debian/Ubuntu and CentOS/RHEL."
+        exit 1
+    fi
+fi
